4c8eb88cef964db3466f2d7601425d6b440bfc91
amanzew <amanzew@gmail.com>
2023-05-20 02:32:25 +0300

    api key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "31d34bfb108f45878172e8435eb77f9c",
+    key: Process.env.REACT_APP_API_KEY,
   },
 });
